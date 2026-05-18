0d283fc7250309f63403f97f63fb810bc99a97e3
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-17 23:39:52 -0400

    feat: add ShortUrl repository domain model

---
+++ src/main/java/io/github/ctorressoftware/urlshortener/application/ports/out/ShortUrlRepository.java
@@ -0,0 +1,10 @@
+package io.github.ctorressoftware.urlshortener.application.ports.out;
+
+import io.github.ctorressoftware.urlshortener.domain.model.ShortUrl;
+
+import java.util.Optional;
+
+public interface ShortUrlRepository {
+    ShortUrl save(ShortUrl shortUrl);
+    Optional<ShortUrl> findByOriginalUrl(String originalUrl);
+}
