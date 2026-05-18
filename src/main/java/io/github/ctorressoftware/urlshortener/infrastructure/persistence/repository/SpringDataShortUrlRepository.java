71c53ce7950462df703304e9c5daeb25e1f70f33
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-17 23:42:23 -0400

    feat: add ShortUrl repository implementation

---
+++ src/main/java/io/github/ctorressoftware/urlshortener/infrastructure/persistence/repository/SpringDataShortUrlRepository.java
@@ -0,0 +1,14 @@
+package io.github.ctorressoftware.urlshortener.infrastructure.persistence.repository;
+
+import io.github.ctorressoftware.urlshortener.infrastructure.persistence.entity.ShortUrlEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface SpringDataShortUrlRepository
+        extends JpaRepository<ShortUrlEntity, UUID> {
+    Optional<ShortUrlEntity> findByOriginalUrl(String originalUrl);
+}
